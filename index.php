2ad8e321ba03c486d9896787d1dbb63ecd012f6a
Matěj <vas@email.com>
2018-03-15 15:25:08 +0100

    přidán <div kuličky >

--- index.php
+++ index.php
@@ -7,15 +7,21 @@
   <body>
     <h1>Proměné</h1>
 <?php
-    define("PI", 3.14) ;
-    $r=5 ;
-    $S= PI*$r*$r;
-    $obvod=  2*PI*$r;
+
+      define("PI", 3.14) ;
+      $r=40 ;
+      $S= PI*$r*$r;
+      $obvod=  2*PI*$r;
 
 
  ?>
- <p> Poloměr : <?php echo $r  ?> </p>
- <p> Obsah kruhu : <?php echo "S = PI * r * r =  " . PI . " * $r * $r ="  ?> <?php echo $S ?> </p>
- <p> Obvod kruhu : O = <?php echo "2 * PI * r =  2 * " . PI . " * $r " ?> = <?php echo $obvod ?></p>
+      <p> Poloměr : <?php echo $r  ?> </p>
+      <p> Obsah kruhu : <?php echo "S = PI * r * r =  " . PI . " * $r * $r ="  ?> <?php echo $S ?> </p>
+      <p> Obvod kruhu : O = <?php echo "2 * PI * r =  2 * " . PI . " * $r " ?> = <?php echo $obvod ?></p>
+
+      <div style="width:<?php echo $obvod?>px ; height:<?php echo $obvod?>px ; border-radius:800px ;
+                  display:block; background:#F00">
+      </div>
+
   </body>
 </html>
